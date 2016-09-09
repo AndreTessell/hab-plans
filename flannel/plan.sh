edfe1122d43266c6b16cb8abc40e9c229cf01290
Ubuntu <ubuntu@ip-172-31-1-147.us-west-2.compute.internal>
2016-09-09 23:50:13 +0000

    hooks

--- flannel/plan.sh
+++ flannel/plan.sh
@@ -5,8 +5,8 @@ pkg_maintainer="Andre Elizondo <andre@chef.io>"
 pkg_license=()
 pkg_source=https://github.com/coreos/flannel/releases/download/v0.6.1/flannel-v0.6.1-linux-amd64.tar.gz
 pkg_shasum=
-pkg_deps=(core/busybox core/go)
-pkg_build_deps=(core/busybox core/go)
+pkg_deps=(core/busybox core/go chefops/etcd)
+pkg_build_deps=(core/busybox core/go chefops/nano)
 pkg_bin_dirs=(bin)
 pkg_include_dirs=(include)
 pkg_lib_dirs=(lib)
@@ -16,8 +16,10 @@ do_build() {
 }
 
 do_install() {
-  return 0
-} 
+  mkdir -p $pkg_prefix/bin
+  cp $HAB_CACHE_SRC_PATH/flanneld $pkg_prefix/bin/flanneld
+  chmod +x $pkg_prefix/bin/flanneld
+}
 
 do_verify() {
   return 0
